c6a089e9fd156063557c3e14e3853e220fe391a0
Julien FOURNIER <julien.fournier@ynov.com>
2018-01-12 08:05:39 +0000

    Update theme.blade.php

--- app/views/template/theme.blade.php
+++ app/views/template/theme.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Bienvenue sur ToqueChef, Site de recettes de cuisine.">
     <title>{{ Config::get('params.appName') }} - @yield('title')</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
