a3af57a40e0194c7ccbdf841ac3761cf104c33d4
Julien FOURNIER <julien.fournier@ynov.com>
2018-01-12 07:42:31 +0000

    Update home.blade.php

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -11,8 +11,10 @@
         recettes de nos utilisateurs sans créer de compte, ou créer un compte pour partager vos recettes, commenter les
         recettes des autres utilisateurs, ou ajouter des recettes à votre portefeuille de recettes.
         <div class="row margin">
-            <a href="{{ route('login') }}" class="btn btn-flat btn-success">Connectez-vous</a>
-            <a href="{{ route('register') }}" class="btn btn-flat btn-warning">Inscrivez-vous</a>
+            @if(!Auth::guest())
+                <a href="{{ route('login') }}" class="btn btn-flat btn-success">Connectez-vous</a>
+                <a href="{{ route('register') }}" class="btn btn-flat btn-warning">Inscrivez-vous</a>
+            @endif
         </div>
     </div>
     <br/>
